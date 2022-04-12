49c92db2c3c62ec1df8a9b0e4593768ef56309f3
enverzeynaliev <fizrukcom62@gmail.com>
2022-04-13 01:41:26 +0300

    Настроено отображение

--- client.py
+++ client.py
@@ -1,25 +1,25 @@
 from server import DH_Endpoint
 
 message = input("Введите текст: ")
-a = 123546978  # по варианту публичный ключ первого пользователя s_public
-x = 1278 # по варианту приватный ключ первого пользователя  s _private
-b = 123978  # по варианту публичный ключ второго пользователя m_public
-y = 6978  # по варианту приватный ключ второго пользователя m _private
+a = 194  # по варианту публичный ключ первого пользователя s_public
+x = 156 # по варианту приватный ключ первого пользователя  s _private
+b = 145  # по варианту публичный ключ второго пользователя m_public
+y = 112  # по варианту приватный ключ второго пользователя m _private
 k1 = 1 # по варианту
 k2 = 1 # по варианту
 user_1 = DH_Endpoint(a, b, x)
 user_2 = DH_Endpoint(a, b, y)
 
 u1_partial = user_1.generate_partial_key()  #полученный защищенный ключ первого пользователя s _partial
-print(u1_partial)
+# print(u1_partial)
 
 u2_partial = user_2.generate_partial_key()  #полученный защищенный ключ второго пользователя m _partial
-print(u2_partial)
+# print(u2_partial)
 
 first_user_full = user_1.generate_full_key(u2_partial)
-print(first_user_full)
+# print(first_user_full)
 m_full = user_2.generate_full_key(u1_partial)
-print(m_full)
+# print(m_full)
 
 msg_encrypted = user_2.encrypt_message(message)
 print(msg_encrypted)
